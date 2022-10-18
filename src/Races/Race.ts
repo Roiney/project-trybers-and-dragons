4e39df645ebd6badc136a096333b98762a125506
Roiney Beal <nei_beal@hotmail.com>
2022-10-18 14:51:15 -0300

    class criada

---
+++ src/Races/Race.ts
@@ -0,0 +1,16 @@
+abstract class Race {
+    name: string;
+    dexterity: number;
+
+ constructor(n: string; d: number){
+    this.name = n;
+    this.dexterity: d;
+ }
+
+ createdRacesInstances():void {
+
+ }
+
+}
+
+export default Race;
