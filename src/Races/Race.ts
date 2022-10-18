349ff4ffb4396570d3855079ce191420bc06eaec
Roiney Beal <nei_beal@hotmail.com>
2022-10-18 15:00:19 -0300

    req 01

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,16 +1,25 @@
 abstract class Race {
-    name: string;
-    dexterity: number;
+  private _name: string;
+  private _dexterity: number;
 
- constructor(n: string; d: number){
-    this.name = n;
-    this.dexterity: d;
- }
+  constructor(n: string, d: number) {
+    this._name = n;
+    this._dexterity = d;
+  }
 
- createdRacesInstances():void {
+  public get name(): string {
+    return this._name;
+  }
 
- }
+  public get dexterity(): number {
+    return this._dexterity;
+  }
 
+  public static createdRacesInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get maxLifePoints(): number;
 }
 
 export default Race;
