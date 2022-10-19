a7bd755f6c8f5c773ee2642549882b0927192695
Roiney Beal <nei_beal@hotmail.com>
2022-10-19 15:11:53 -0300

    req 09

--- src/Character.ts
+++ src/Character.ts
@@ -1,5 +1,6 @@
 import Archetypes, { Mage } from './Archetypes';
 import Energy from './Energy';
+import { SimpleFighter } from './Fighter';
 import Fighter from './Fighter/Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
@@ -70,7 +71,7 @@ class Character implements Fighter {
     return this._lifePoints;
   }
   
-  public attack(enemy: Fighter): void {
+  public attack(enemy: Fighter | SimpleFighter): void {
     const damage = this._strength;
     enemy.receiveDamage(damage);
   }
